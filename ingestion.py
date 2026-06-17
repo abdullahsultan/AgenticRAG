d27aa50f320021866d325b1ae24b921f6c7f952a
BigBrownBear <abdullahsultan448@gmail.com>
2026-06-17 22:17:06 +0400

    Ingestion

--- ingestion.py
+++ ingestion.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from langchain_text_splitters  import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 load_dotenv()
 
@@ -11,3 +11,22 @@ urls = [
     "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
     "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
 ]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=250, chunk_overlap=0)
+
+docs_split = text_splitter.split_documents(docs_list)
+
+vector_store = Chroma.from_documents(
+    documents=docs_split,
+    collection_name="rag-chroma",
+    embedding=OllamaEmbeddings(model="qwen3-embedding"),
+    persist_directory="./.chroma_db",
+    )
+
+retriever = Chroma(collection_name="rag-chroma",
+                 persist_directory="./.chroma_db",
+                 embedding_function=OllamaEmbeddings(model="qwen3-embedding"),
+                 ).as_retriever()
